e872d540a156951ea5ca5144691b4736d57d5011
UNF-GDC <109671887+UNF-GDC@users.noreply.github.com>
2022-07-24 20:29:52 +0200

    Omrokering af filstruktur
    Tak for inputs :100: <3

--- GDC-project/Assets/Script/SelectionScript.cs
+++ GDC-project/Assets/Script/SelectionScript.cs
@@ -7,30 +7,42 @@ public class SelectionScript : MonoBehaviour
     // Start is called before the first frame update
 
     Camera cam;
+    
+    
     void Start()
     {
         cam = GetComponent<Camera>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Layer 3 is the book layer
-        int layerMask = LayerMask.GetMask("Book");
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Layer 3 is the book layer
+            int layerMask = LayerMask.GetMask("Book");
+            
 
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity,layerMask))
-        {
-            Debug.Log("Did Hit");
-            Transform hitTransform = hit.collider.gameObject.GetComponent<Transform>();
-            hitTransform.Rotate(Vector3.forward * 7);
-        }
-        else
-        {
-            Debug.Log("Did not Hit");
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+
+            
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.Log("Did Hit");
+
+                
+
+                print("YAAAY!");
+                // gør bogen lysere (sæt HUE på materialet)
+            }
+            else
+            {
+                Debug.Log("Did not Hit");
+            }
         }
     }
 }
