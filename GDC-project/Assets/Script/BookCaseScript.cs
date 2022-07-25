bc53e83d5772c2db9b7644844b5fcaca915af114
UNF-GDC <109671887+UNF-GDC@users.noreply.github.com>
2022-07-25 20:20:54 +0200

    Bøger skifter plads (uden at vælte)
    YEEAEEAEESESESA :100:

--- GDC-project/Assets/Script/BookCaseScript.cs
+++ GDC-project/Assets/Script/BookCaseScript.cs
@@ -7,17 +7,13 @@ public class BookCaseScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        {
-           
-            
-        }
+        
     }
 }
 
